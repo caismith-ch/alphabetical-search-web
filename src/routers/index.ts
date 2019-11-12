daf7e0bafae1ab4e541cb266173a44f4c415d7cf
caismith-ch <csmith2@companieshouse.gov.uk>
2019-11-12 10:20:26 +0000

    Testing code for returning json from an external api required for returning response from elastic search

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,22 @@
 import {Router} from "express";
 import {searchHandler} from "../handlers";
 
+const request = require('request');
+
 // a router is a collection of routes that can have their own middleware chain. It is helpful to create routers for
 // a collection of related routes for better organisation and specific logic.
 const router = Router();
 router.get("/", searchHandler);
+router.get("/get-results", () => {
 
+    request('http://localhost:8080/movies', {json: true}, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(body);
+        body.forEach(element => {
+        console.log(element.title);
+        });
+    });
+
+});
 
 export default router;
